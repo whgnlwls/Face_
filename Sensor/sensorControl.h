af3dcafdfd10d2841ffb1eebc8bb8d6c2bce67e2
Greedy <37136964+whgnlwls@users.noreply.github.com>
2021-07-11 20:11:22 +0900

    Update sensorControl.h

--- Sensor/sensorControl.h
+++ Sensor/sensorControl.h
@@ -1,54 +1,9 @@
 //Buzzer
-void BZsetBuzzer(int buzzer) {
-	//NoT = Number of Times
-	for(int NoT = 0; NoT < 10; NoT++) {
-		for(int frequency = 0; frequency < 100; frequency++) {
-		digitalWrite(buzzer, LOW);
-		delay(2);
-		digitalWrite(buzzer, HIGH);
-		delay(2);
-		}
-		//Blank
-		delay(100);
-	}
-}
-//UltraSonic
-float USgetDist(int ultraSonicTrig, int ultraSonicEcho) {
-	float distance;
-	int throw_time, catch_time;
-	
-	for(int i = 0; i < 5; i++) {
-    digitalWrite(ultraSonicTrig, LOW);
-    delay(200);
-    
-    //throw
-    digitalWrite(ultraSonicTrig, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(ultraSonicTrig, LOW);
-    
-    //wait
-    while (digitalRead(ultraSonicEcho) == 0);
-    
-    //if catch signal
-    throw_time = micros();
-    while (digitalRead(ultraSonicEcho) == 1);
-    catch_time = micros();
-    
-    distance += (catch_time - throw_time) / 29. / 2.;
-  }
-  distance = distance / 5;
-  printf("distance %.2fcm\n", distance);
-  
-  return distance;
-}
-//Motor
-void MTsetOpen() {
-	softPwmCreate(MOT, 0, 200);
+void BZsetBuzzer(int buzzer);
 
-	softPwmWrite(MOT,5);
-}
-void MTsetClose() {
-	softPwmCreate(MOT, 0, 200);
-	
-	softPwmWrite(MOT,24);
-}
+//UltraSonic
+float USgetDist(int ultraSonicTrig, int ultraSonicEcho);
+
+//Motor
+void MTsetOpen();
+void MTsetClose();
