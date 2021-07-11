d71a6d227f307a2efa0ca6b6743296b3bb4cb2ab
Greedy <37136964+whgnlwls@users.noreply.github.com>
2021-07-11 16:58:26 +0900

    Update sensorControl.h

--- Sensor/sensorControl.h
+++ Sensor/sensorControl.h
@@ -1,1 +1,28 @@
-
+float USgetDist(int ultraSonicTrig, int ultraSonicEcho) {
+	float distance;
+	int throw_time, catch_time;
+	
+	for(int i = 0; i < 5; i++) {
+    digitalWrite(ultraSonicTrig, LOW);
+    delay(200);
+    
+    //throw
+    digitalWrite(ultraSonicTrig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(ultraSonicTrig, LOW);
+    
+    //wait
+    while (digitalRead(ultraSonicEcho) == 0);
+    
+    //if catch signal
+    throw_time = micros();
+    while (digitalRead(ultraSonicEcho) == 1);
+    catch_time = micros();
+    
+    distance += (catch_time - throw_time) / 29. / 2.;
+  }
+  distance = distance / 5;
+  printf("distance %.2fcm\n", distance);
+  
+  return distance;
+}
