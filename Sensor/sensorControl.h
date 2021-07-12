6c216a6d20f028f6bfd64a7f7029d6c38f712b87
Greedy <37136964+whgnlwls@users.noreply.github.com>
2021-07-12 23:38:49 +0900

    Update sensorControl.h

--- Sensor/sensorControl.h
+++ Sensor/sensorControl.h
@@ -1,3 +1,9 @@
+//header guard
+#ifndef SENSORCONTROL_H
+#define SENSORCONTROL_H
+
+출처: https://boycoding.tistory.com/146 [소년코딩]
+
 //Buzzer
 extern void BZsetBuzzer(int buzzer);
 
@@ -7,3 +13,5 @@ extern float USgetDist(int ultraSonicTrig, int ultraSonicEcho);
 //Motor
 extern void MTsetOpen(int motor);
 extern void MTsetClose(int motor);
+
+#endif
