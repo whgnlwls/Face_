bb881187e232b8d42e6727a3b99e0ab1be8cdc99
Greedy <37136964+whgnlwls@users.noreply.github.com>
2021-07-12 23:58:41 +0900

    Update sensorControl.h

--- Sensor/sensorControl.h
+++ Sensor/sensorControl.h
@@ -2,16 +2,31 @@
 #ifndef SENSORCONTROL_H
 #define SENSORCONTROL_H
 
-출처: https://boycoding.tistory.com/146 [소년코딩]
-
-//Buzzer
-void BZsetBuzzer(int buzzer);
-
-//UltraSonic
-float USgetDist(int ultraSonicTrig, int ultraSonicEcho);
-
-//Motor
-void MTsetOpen(int motor);
-void MTsetClose(int motor);
+//class
+class DoorLock {
+  private:
+  int pinBuzzer;
+  int pinUltraSonicTrig;
+  int pinUltraSonicEcho;
+  int pinMotor;
+  
+  float Dist;
+  
+  protected:
+  
+  public:
+  //initialize
+  DoorLock(int pinBuzzer, int pinUltraSonicTrig, pinUltraSonicEcho, pinMotor);
+  
+  //Buzzer
+  void BZsetBuzzer(int buzzer);
+  
+  //UltraSonic
+  float USgetDist(int ultraSonicTrig, int ultraSonicEcho) {return Dist};
+  
+  //Motor
+  void MTsetOpen(int motor);
+  void MTsetClose(int motor);
+}
 
 #endif
