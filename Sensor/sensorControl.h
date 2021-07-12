3e440b6929e4911d0fa3618f1f7eb9e8d596a6e8
Greedy <37136964+whgnlwls@users.noreply.github.com>
2021-07-12 23:39:46 +0900

    Update sensorControl.h

--- Sensor/sensorControl.h
+++ Sensor/sensorControl.h
@@ -5,13 +5,13 @@
 출처: https://boycoding.tistory.com/146 [소년코딩]
 
 //Buzzer
-extern void BZsetBuzzer(int buzzer);
+void BZsetBuzzer(int buzzer);
 
 //UltraSonic
-extern float USgetDist(int ultraSonicTrig, int ultraSonicEcho);
+float USgetDist(int ultraSonicTrig, int ultraSonicEcho);
 
 //Motor
-extern void MTsetOpen(int motor);
-extern void MTsetClose(int motor);
+void MTsetOpen(int motor);
+void MTsetClose(int motor);
 
 #endif
