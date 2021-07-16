0ddabf9fc46ca444ba6a59712f93cd74a7cc65ca
Greedy <37136964+whgnlwls@users.noreply.github.com>
2021-07-16 14:12:09 +0900

    Update test.cpp

--- Sensor/test.cpp
+++ Sensor/test.cpp
@@ -3,20 +3,20 @@
 #include <softPwm.h>
 #include "sensorControl.h"
 
-#define buz
-#define ulst
-#define ulse
-#define mot
+#define buz 21
+#define ulst 28
+#define ulse 27
+#define mot 26
 
 int main() {
-  include main method if(wiringPiSetup() == -1) return -1;
-  
-  DoorLock doorlock = new DoorLock(buz, ulst, ulse, mot);
-  
-  doorlock.BZsetBuzzer();
-  doorlock.USgetDist();
-  doorlock.MTsetOpen();
-  doorlock.MTsetClose();
-  
-  return 0;
+	if (wiringPiSetup() == -1) return -1;
+
+	DoorLock doorlock = DoorLock(buz, ulst, ulse, mot);
+
+	doorlock.BZsetBuzzer();
+	doorlock.USgetDist();
+	doorlock.MTsetOpen();
+	doorlock.MTsetClose();
+
+	return 0;
 }
