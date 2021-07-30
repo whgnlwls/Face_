445fbe18e279e4b66d5e001786b0c9a99009ef6f
Greedy <37136964+whgnlwls@users.noreply.github.com>
2021-07-30 22:19:15 +0900

    Create DoorLockHead.h
    all header files

---
+++ DoorLockHead.h
@@ -0,0 +1,18 @@
+#ifndef DOORLOCKHEAD_H
+#define DOORLOCKHEAD_H
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <stdio.h>
+#include <wiringPi.h>
+#include <softPwm.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <WinSock2.h>
+#pragma comment (lib , "ws2_32.lib")
+#include <Windows.h>
+
+using namespace std;
+
+#endif
