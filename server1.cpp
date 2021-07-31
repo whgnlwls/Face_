be527aa25fc99b303d7861e3eea9036e9807d54c
Greedy <37136964+whgnlwls@users.noreply.github.com>
2021-07-31 20:37:51 +0900

    Create server1.cpp

---
+++ server1.cpp
@@ -0,0 +1,29 @@
+#include "server.h"
+
+#define BUFSIZE 512
+
+//initialize
+//create socket
+Server::Server(int PORT) {
+	
+}
+
+//close socket
+Server::~Server() {
+	
+}
+
+//bind
+void Server::bindSocket() {
+	
+}
+
+//listen
+void Server::listenSocket() {
+	
+}
+
+//accept
+void Server::acceptSocket() {
+	
+}
