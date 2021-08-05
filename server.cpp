e480750a7ddb644740153e38bf4813e1a79a524f
Greedy <37136964+whgnlwls@users.noreply.github.com>
2021-08-05 15:48:35 +0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -8,6 +8,7 @@ Server::Server(int PORT) {
 	}
 	cout << "create server socket success" << endl;
 	
+	memset(&serverAddr, 0, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(PORT);
 	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
