6276020cff473e9ed9f828162d4538ef5f830e1b
Greedy <37136964+whgnlwls@users.noreply.github.com>
2021-09-15 16:46:53 +0900

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,24 +1,16 @@
-#include "sensorControl.h"
 #include "server.h"
 
 #define buz 21
-#define ulst 28
-#define ulse 27
+#define fulst 28
+#define fulse 27
+#define bulst 22
+#define bulse 23
 #define mot 26
 
 int main() {
 	if (wiringPiSetup() == -1) return -1;
-	/*
-	sensorControl.BZsetBuzzer();
-	for(int i = 0; i < 5; i++) {
-		sensorControl.USgetDist();
-	}
-	sensorControl.MTsetOpen();
-	sensorControl.MTsetClose();
-	*/
-	SensorControl sensor = SensorControl(buz, ulst, ulse, mot);
-	Server server = Server(9000);
 	
+	Server server(buz, fulst, fulse, bulst, bulse, mot, 9000);
 	printf("Finish\n");
 
 	return 0;
